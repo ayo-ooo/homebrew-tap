2fed052eb92b7eef9be86dcc40c7a5884a772d63
Alex Cabrera <email@alexcabrera.me>
2026-03-19 19:59:37 -0400

    bump to v0.1.1

--- Formula/ayo.rb
+++ Formula/ayo.rb
@@ -1,14 +1,11 @@
-# typed: false
-# frozen_string_literal: true
-
 class Ayo < Formula
   desc "CLI tool for building AI agents with structured inputs and outputs"
   homepage "https://github.com/ayo-ooo/ayo"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   url "https://github.com/ayo-ooo/ayo/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "81722d6f76c97cfce317a6acc348f7372801f661f3b70f066676212c959addf7"
+  sha256 "ba5051254a882830867cca7751c66382d3fdc9d961921beaa03f639ed6be56a0"
 
   depends_on "go" => :build
 
