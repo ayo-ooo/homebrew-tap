ba59579c773f8de878327bda5389349e71d236dd
Alex Cabrera <email@alexcabrera.me>
2026-03-19 16:34:22 -0400

    Update formula SHA256 for v0.1.0

--- Formula/ayo.rb
+++ Formula/ayo.rb
@@ -7,28 +7,10 @@ class Ayo < Formula
   version "0.1.0"
   license "MIT"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/ayo-ooo/ayo/archive/refs/tags/v#{version}.tar.gz"
-      sha256 "ae51936325465234e065083f8fcb6950a562880d2593834f8d50a82f4308a92d"
+  url "https://github.com/ayo-ooo/ayo/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "81722d6f76c97cfce317a6acc348f7372801f661f3b70f066676212c959addf7"
 
-      depends_on "go" => :build
-    end
-
-    on_intel do
-      url "https://github.com/ayo-ooo/ayo/archive/refs/tags/v#{version}.tar.gz"
-      sha256 "ae51936325465234e065083f8fcb6950a562880d2593834f8d50a82f4308a92d"
-
-      depends_on "go" => :build
-    end
-  end
-
-  on_linux do
-    url "https://github.com/ayo-ooo/ayo/archive/refs/tags/v#{version}.tar.gz"
-    sha256 "ae51936325465234e065083f8fcb6950a562880d2593834f8d50a82f4308a92d"
-
-    depends_on "go" => :build
-  end
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/ayo"
